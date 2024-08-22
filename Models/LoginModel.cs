80ea81838e7072b3f7f4afc968dfb8b47a0cf5bb
David Guragain <davidguragain@Davids-MacBook-Air.local>
2024-08-22 10:57:39 +0545

    learning project

---
+++ Models/LoginModel.cs
@@ -0,0 +1,8 @@
+namespace BlazorApp.Models  // Ensure 'BlazorApp' matches your project name
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
